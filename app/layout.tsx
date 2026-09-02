35638a483b9a8a06fcd53dcceb8c33b12304276a
Webboomx00 <kevinadrians00@gmail.com>
2026-09-02 09:19:29 +0700

    v2 stock opname and label format fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SiteNav from "@/components/Sitenav";
+import SiteNav from "@/components/SiteNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
